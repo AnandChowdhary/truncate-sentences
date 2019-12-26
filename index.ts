78e41809d06697f0468b0343ad1d90e099018533
Anand Chowdhary <mail@anandchowdhary.com>
2019-12-26 13:12:59 -0500

    :sparkles: Add tokenizer

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-export default () => "";
+import natural from "natural";
+const SentenceTokenizer = new (natural as any).SentenceTokenizer() as natural.WordTokenizer;
+
+export default (text: string, limit: number = 300) => {
+  const tokens = SentenceTokenizer.tokenize(text);
+  let result = "";
+  let length = 0;
+  for (const token of tokens) {
+    if (length + token.length <= limit) {
+      result += ` ${token}`;
+      length += token.length;
+    } else break;
+  }
+  return result || (text.length > limit ? `${text.substring(0, limit)}...` : text);
+}
