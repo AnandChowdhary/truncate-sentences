5833ab2c2911bc87efeb52a9e3acd906fbfbbddf
Anand Chowdhary <mail@anandchowdhary.com>
2022-11-22 10:34:21 +0100

    :sparkles: Add support for custom ellipsis

--- index.test.ts
+++ index.test.ts
@@ -1,25 +1,53 @@
 import truncate from "./index";
 
-const paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum. Nullam at diam felis. Sed vitae iaculis sem. Duis scelerisque vehicula malesuada. Nullam sodales eget tortor tincidunt faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper purus at leo interdum, nec egestas massa vestibulum. Pellentesque ornare eget tellus vel malesuada. Sed et orci quam. Quisque fermentum lacus vitae erat volutpat, et finibus turpis vulputate. Donec ultrices pretium rhoncus. Aliquam vitae blandit enim, non gravida nibh. Integer at lorem convallis, pulvinar ligula vitae, blandit ligula.";
-
-const sentence = "Lorem ipsum dolor sit amet, consectetur adipiscing elit curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum nullam at diam felis sed vitae iaculis sem duis scelerisque vehicula malesuada nullam sodales eget tortor tincidunt faucibus lorem ipsum dolor sit amet, consectetur adipiscing elit mauris semper purus at leo interdum, nec egestas massa vestibulum pellentesque ornare eget tellus vel malesuada sed et orci quam quisque fermentum lacus vitae erat volutpat, et finibus turpis vulputate donec ultrices pretium rhoncus aliquam vitae blandit enim, non gravida nibh integer at lorem convallis, pulvinar ligula vitae, blandit ligula.";
-
 test("truncates a paragraph", () => {
-  expect(truncate(paragraph)).toBe("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum. Nullam at diam felis. Sed vitae iaculis sem. Duis scelerisque vehicula malesuada. Nullam sodales eget tortor tincidunt faucibus.");
+  expect(
+    truncate(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum. Nullam at diam felis. Sed vitae iaculis sem. Duis scelerisque vehicula malesuada. Nullam sodales eget tortor tincidunt faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper purus at leo interdum, nec egestas massa vestibulum. Pellentesque ornare eget tellus vel malesuada. Sed et orci quam. Quisque fermentum lacus vitae erat volutpat, et finibus turpis vulputate. Donec ultrices pretium rhoncus. Aliquam vitae blandit enim, non gravida nibh. Integer at lorem convallis, pulvinar ligula vitae, blandit ligula."
+    )
+  ).toBe(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum. Nullam at diam felis. Sed vitae iaculis sem. Duis scelerisque vehicula malesuada. Nullam sodales eget tortor tincidunt faucibus."
+  );
 });
 
 test("truncates a paragraph to a specific length", () => {
-  expect(truncate(paragraph, 100)).toBe("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+  expect(
+    truncate(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum. Nullam at diam felis. Sed vitae iaculis sem. Duis scelerisque vehicula malesuada. Nullam sodales eget tortor tincidunt faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper purus at leo interdum, nec egestas massa vestibulum. Pellentesque ornare eget tellus vel malesuada. Sed et orci quam. Quisque fermentum lacus vitae erat volutpat, et finibus turpis vulputate. Donec ultrices pretium rhoncus. Aliquam vitae blandit enim, non gravida nibh. Integer at lorem convallis, pulvinar ligula vitae, blandit ligula.",
+      100
+    )
+  ).toBe("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
 });
 
 test("truncates a sentence", () => {
-  expect(truncate(sentence)).toBe("Lorem ipsum dolor sit amet, consectetur adipiscing elit curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum nullam at diam felis sed vitae iaculis sem duis scelerisque vehicula malesuada nullam sodales eget tortor tincidunt faucibus lorem ipsum dolor sit amet, consectetur adipisci...");
+  expect(
+    truncate(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum nullam at diam felis sed vitae iaculis sem duis scelerisque vehicula malesuada nullam sodales eget tortor tincidunt faucibus lorem ipsum dolor sit amet, consectetur adipiscing elit mauris semper purus at leo interdum, nec egestas massa vestibulum pellentesque ornare eget tellus vel malesuada sed et orci quam quisque fermentum lacus vitae erat volutpat, et finibus turpis vulputate donec ultrices pretium rhoncus aliquam vitae blandit enim, non gravida nibh integer at lorem convallis, pulvinar ligula vitae, blandit ligula."
+    )
+  ).toBe(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum nullam at diam felis sed vitae iaculis sem duis scelerisque vehicula malesuada nullam sodales eget tortor tincidunt faucibus lorem ipsum dolor sit amet, consectetur adipisci…"
+  );
 });
 
 test("truncates a sentence to a specific length", () => {
-  expect(truncate(sentence, 100)).toBe("Lorem ipsum dolor sit amet, consectetur adipiscing elit curabitur ipsum eros, scelerisque id volutpa...");
+  expect(
+    truncate(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum nullam at diam felis sed vitae iaculis sem duis scelerisque vehicula malesuada nullam sodales eget tortor tincidunt faucibus lorem ipsum dolor sit amet, consectetur adipiscing elit mauris semper purus at leo interdum, nec egestas massa vestibulum pellentesque ornare eget tellus vel malesuada sed et orci quam quisque fermentum lacus vitae erat volutpat, et finibus turpis vulputate donec ultrices pretium rhoncus aliquam vitae blandit enim, non gravida nibh integer at lorem convallis, pulvinar ligula vitae, blandit ligula.",
+      100
+    )
+  ).toBe(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit curabitur ipsum eros, scelerisque id volutpa…"
+  );
 });
 
-test("truncates a paragraph to a specific length with ellipsis", () => {
-  expect(truncate(paragraph, 100, true)).toBe("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ipsum eros, scelerisque id volutp...");
-});
+// KNOWN ISSUE
+// test("truncates a paragraph to a specific length", () => {
+//   expect(
+//     truncate(
+//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. This is some example copy. Ok, said Dr. Feynman. Curabitur ipsum eros, scelerisque id volutpat nec, lobortis quis ipsum. Nullam at diam felis. Sed vitae iaculis sem. Duis scelerisque vehicula malesuada. Nullam sodales eget tortor tincidunt faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper purus at leo interdum, nec egestas massa vestibulum. Pellentesque ornare eget tellus vel malesuada. Sed et orci quam. Quisque fermentum lacus vitae erat volutpat, et finibus turpis vulputate. Donec ultrices pretium rhoncus. Aliquam vitae blandit enim, non gravida nibh. Integer at lorem convallis, pulvinar ligula vitae, blandit ligula.",
+//       100
+//     )
+//   ).toBe(
+//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, said. This is some example copy."
+//   );
+// });
--- index.ts
+++ index.ts
@@ -1,19 +1,33 @@
 import natural from "natural";
-const SentenceTokenizer = new (natural as any).SentenceTokenizer() as natural.WordTokenizer;
+const SentenceTokenizer = new natural.SentenceTokenizer();
 
-export default (text: string, limit = 300, forceEllipsis = false) => {
+export default (
+  text: string,
+  limit = 300,
+  ellipsisCharacter = "…" // or "..."
+) => {
+  // One space after punctuation
+  text = text.replace(/([.?!])\s*(?=[A-Z])/g, "$1 ");
+
   const tokens = SentenceTokenizer.tokenize(text);
-  let result = "";
+
+  // If the text is short enough, return it as is
+  if (text.length < limit) return text;
+
+  // Calculate recommended length taking sentences into consideration
   let length = 0;
-  if (!forceEllipsis)
-    for (const token of tokens) {
-      if (length + token.length <= limit) {
-        result += `${token} `;
-        length += token.length;
-      } else break;
-    }
-  return (
-    result.trim() ||
-    (text.length > limit ? `${text.substring(0, limit)}...` : text)
-  );
+  for (const token of tokens) {
+    if (length + token.length <= limit) {
+      length += token.length + 1;
+    } else break;
+  }
+
+  // If first sentence is longer than limit, return ellipsis
+  if (length === 0)
+    return `${text.substring(0, limit).trim()}${ellipsisCharacter}`.replaceAll(
+      `.${ellipsisCharacter}`,
+      ellipsisCharacter
+    );
+
+  return text.substring(0, length).trim();
 };
